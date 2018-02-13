4ed5253b969dcfd9b8ca0927b5bd022dc6a09edd
Felipe Pontes <felipemfpontes@gmail.com>
2018-02-13 12:07:47 -0300

    Add antd

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import logo from '../../assets/logo.svg'
-import './index.css'
 
 import Home from '../Home'
 import About from '../About'
@@ -15,14 +14,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <Router>
-            <React.Fragment>
-              <Route exact path="/" component={Home}/>
-              <Route path="/sobre" component={About}/>
-            </React.Fragment>
-          </Router>
-        </p>
+        <Router>
+          <React.Fragment>
+            <Route exact path="/" component={Home}/>
+            <Route path="/sobre" component={About}/>
+          </React.Fragment>
+        </Router>
       </div>
     )
   }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Button from 'antd/lib/button'
+
 
 const Home = () => {
   return (
     <div>
-      SIAC
+      <p>Sistema Interativo de Avaliação de Conhecimento</p>
+      <Button type="primary">Continuar</Button>
     </div>
   )
 }
