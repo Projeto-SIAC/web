73aefa881d3c44311fd6b0d77a50549c3b5b54db
Felipe Pontes <felipemfpontes@gmail.com>
2018-02-24 19:00:34 -0300

    Add LoginPage UI

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Redirect } from 'react-router-dom'
 
+import { 
+  Button,
+  Divider,
+  Form, 
+  Icon,
+  Input,
+} from 'antd'
+
 import Auth from 'utils/auth'
+
+import LogoSrc from 'assets/images/logo.svg'
+
+
+const FormItem = Form.Item;
 
 const Container = styled.div`
   height: 100vh;
@@ -10,20 +23,49 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  background: #f5f5f5;
 `
 
-const Form = styled.form`
-  max-width: 300px;
+const Box = styled.main`
+  width: 20rem;
+  box-shadow: 0rem 0.5rem 1.5rem -0.25rem rgba(25,25,35,0.25);
+  border-radius: 0.35rem;
+  padding: 2.5rem;
+  background-color: #fff;
 `
 
-export default class Login extends Component {
+const FixedButton = styled(Button)`
+  width: 100%
+`
+
+const PlaceholderIcon = styled(Icon)`
+  color: rgba(0,0,0,.25);
+`
+
+const Image = styled.img`
+  display: block;
+  margin: 0 auto;
+  height: 3rem;
+  width: 3rem;
+`
+
+class LoginPage extends Component {
   state = {
     redirectToReferrer: false
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    this.props.form.validateFields((err, values) => {
+      if (!err) console.log('Received values of form: ', values)
+    })
   }
 
   render() {
     const { from } = this.props.location.state || { from: { pathname: "/" } }
     const { redirectToReferrer } = this.state
+    const { getFieldDecorator } = this.props.form
 
     if (redirectToReferrer) {
       return <Redirect to={from} />
@@ -31,11 +73,34 @@ export default class Login extends Component {
 
     return (
       <Container>
-        <h1>SIAC</h1>
-        <Form>
-          <button onClick={() => Auth.authenticate(() => this.setState({redirectToReferrer: true}))}>Login</button>
-        </Form>
+        <Box>
+          <Image src={LogoSrc} alt="Logo do SIAC" />
+          <Divider />
+          <Form onSubmit={this.handleSubmit}>
+            <FormItem hasFeedback>
+              {getFieldDecorator('username', {
+                rules: [{required: true, message: 'Informe sua matrícula!'}]
+              })(
+                <Input prefix={<PlaceholderIcon type="user" />} placeholder="Matrícula" />
+              )}
+            </FormItem>
+            <FormItem hasFeedback>
+              {getFieldDecorator('password', {
+                rules: [{required: true, message: 'Informe sua senha!'}]
+              })(
+                <Input prefix={<PlaceholderIcon type="lock" />} type="password" placeholder="Senha" />
+              )}
+            </FormItem>
+            <FormItem>
+              <FixedButton type="primary" htmlType="submit">
+                Acessar
+              </FixedButton>
+            </FormItem>
+          </Form>
+        </Box>
       </Container>
     )
   }
 }
+
+export default Form.create()(LoginPage)
