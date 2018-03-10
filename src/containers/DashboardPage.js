de4135de28ff37a2896e82d0823f70e79752011a
Felipe Pontes <felipemfpontes@gmail.com>
2018-03-10 12:09:50 -0300

    Add User available to children by context

--- src/containers/DashboardPage.js
+++ src/containers/DashboardPage.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import { Breadcrumb, Icon, Row, Col } from 'antd'
 import PageLayout from 'components/PageLayout'
 import IconCard from 'components/IconCard'
 
-const DashboardPage = () => {
+const DashboardPage = (props, context) => {
   return (
     <PageLayout breadcrumb={() => <Breadcrumb style={{ margin: '16px 0' }}>
         <Breadcrumb.Item><Icon type="home" /></Breadcrumb.Item>
         <Breadcrumb.Item>Ínicio</Breadcrumb.Item>
       </Breadcrumb>
     }>
-      <h1>O que vamos fazer?</h1>
+      <h1>O que vamos fazer, {context.user.firstName}?</h1>
       <Row type="flex" justify="space-around" align="middle" gutter={24} style={{minHeight: 200}}>
         <Col span={24} md={12} lg={6} style={{padding: '0.5rem 0.5rem'}}><IconCard title="Realizar autoavaliação" type="file-text" size={36} /></Col>
         <Col span={24} md={12} lg={6} style={{padding: '0.5rem 0.5rem'}}><IconCard title="Nova avaliação" type="file-add" size={36} /></Col>
@@ -20,6 +21,10 @@ const DashboardPage = () => {
       </Row>
     </PageLayout>
   )
+}
+
+DashboardPage.contextTypes = {
+  user: PropTypes.object
 }
 
 export default DashboardPage
